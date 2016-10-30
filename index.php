2afd87bf570b8dc342586a82a7496266f0da5720
Bart <bart@tuxion.nl>
2016-10-30 10:14:35 +0100

    First version

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <meta name="viewport" content="width=device-width">
+
+  <title>Cryptosteden in Nederland - Hier betaal je met Bitcoin</title>
+
+</head>
+
+<frameset cols="200, *" frameborder="0" border="0">
+  <frame src="menu.php" name="menu" scrolling="auto" noresize></frame>
+  <frame src="content.php" name="content" scrolling="auto" noresize></frame>
+</frameset>
+
+</html>
---
+++ menu.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+  <title>cryptostad menu</title>
+
+  <style type="text/css">
+    * {
+      box-sizing: border-box;
+    }
+    body, html {
+      min-height: 100%;
+      height: 100%;
+      overflow: auto;
+      margin: 0;
+      padding: 0;
+    }
+    body {
+      border-right: solid #f38b31 2px;
+      font-family: sans-serif;
+    }
+    strong {
+      display: block;
+      padding: 5px 10px;
+      color: #f38b31;
+    }
+    nav {
+      margin: 20px 0;
+    }
+    a {
+      display: block;
+      margin:  2px 0;
+      background-color: #eee;
+      padding: 10px 8px;
+      color: #000;
+      text-decoration: none;
+      border-bottom: solid #f38b31 1px;
+    }
+    a:hover,
+    a:active,
+    a:focus {
+      background-color: #f38b31;
+      color: #000;
+    }
+  </style>
+</head>
+<body>
+
+<nav class="places">
+  <strong>Plaatsen</strong>
+  <a href="http://www.arnhembitcoinstad.nl/" target="content">Arnhem</a>
+  <a href="http://gentbitcoinstad.be/" target="content">Gent</a>
+  <a href="https://coinmap.org/?amsterdam#/map/52.37224557/4.91878510/13" target="content">Amsterdam</a>
+  <a href="https://coinmap.org/?denhaag#/map/52.07354500/4.30149078/14" target="content">Den Haag</a>
+  <a href="https://coinmap.org/?all#/map/52.37224557/4.91878510/13" target="content">Andere plaatsen</a>
+</nav>
+
+<nav class="apps">
+  <strong>Apps</strong>
+  <a href="http://bitmapapp.com/" target="content">Bitmap voor iOS</a>
+</nav>
+
+</body>
+</html>
