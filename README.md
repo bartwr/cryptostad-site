13874b970ef60db45c2a5f8c8673cd745f108df4
Bart <bart@tuxion.nl>
2016-10-30 13:03:59 +0100

    Added an introduction text. Also, created README.md

---
+++ README.md
@@ -0,0 +1,9 @@
+# cryptostad-site
+
+Cryptostad geeft informatie over plaatsen die voorop lopen in de acceptatie van bitcoin en ander modern geld.
+
+Wil je bijdragen aan de ontwikkeling van de site? Pas dan de code aan en doe een <i>pull request</i>.
+
+Een mailtje sturen naar <a href="mailto:mail@bartroorda.nl">mail@bartroorda.nl</a> kan ook.
+
+Bart
---
+++ content.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title></title>
+
+  <link rel="stylesheet" type="text/css" href="style.css" />
+
+  <style type="text/css">
+    body {
+      padding: 20px 40px;
+      line-height: 1.4em
+    }
+  </style>
+
+</head>
+<body>
+
+<h1>cryptostad.nl</h1>
+
+<p>
+  <i>Cryptocurrencies</i> worden overal ter wereld gebruikt. De adoptie is nog klein doch verloopt exponentieel. Zodra je bekend bent met de voordelen zul je niet snel terug willen naar oude betaalmethodes.
+</p>
+
+<p>
+  Vaak wordt gevraagd: waar kan ik betalen met bitcoins of ander cryptogeld? Dat kan op heel veel plekken, vooral online. Fysiek wordt het vooral gebruikt in de <i>unbanked</i> wereld.
+</p>
+
+<p>
+  Gelukkig beginnen er ook in Nederland beginnen steeds meer fysieke plekken te komen waar je met bitcoins kunt betalen. Kijk bijvoorbeeld naar Arnhem en Amsterdam, waar meer dan 100 plekken zijn waar je met bitcoin betaalt. Café's, restaurants, schoenenwinkels, bakkers. Zelfs de <a href="https://www.cryptocoinsnews.com/first-bitcoin-supermarket-arnhem-netherlands/" target="_blank">Spar</a>, BP en Burger King doen mee.
+</p>
+
+<p>
+  Op deze site een overzicht van sites die inzicht geven in de fysieke, Nederlandse en Belgische plekken waar je met bitcoin kunt betalen. Klik links op een van de plaatsen om te beginnen.
+</p>
+
+<hr />
+
+<p>
+  <small>Deze site is open source en daarmee te verbeteren door iedereen. Zie <a href="https://github.com/bartwr/cryptostad-site" target="_blank">github.com/bartwr/cryptostad-site</a></small>
+</p>
+
+</body>
+</html>
